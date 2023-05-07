fea3fadec24e1ced99843c21853fdd931833718e
juaneslava <j.eslava@uniandes.edu.co>
2023-05-07 15:27:45 -0500

    taller angular

---
+++ tallerangular/src/app/serie/serie.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { dataSeries } from './dataSeries';
+import { SerieService } from './serie.service';
+
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  series: Array<Serie> = [];
+  constructor(private serieService: SerieService) { }
+
+  getSerieList(): Array<Serie> {
+    return dataSeries;
+  }
+
+  getCourses() {
+    this.serieService.getSeries().subscribe(series => {
+      this.series = series;
+    });
+}
+
+  ngOnInit() {
+    this.series = this.getSerieList();
+  }
+
+}
